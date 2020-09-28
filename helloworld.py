50a80e4d4d1785b3d269fa908fc746be8656a5ed
unknown <allison@alunos.utfpr.edu.br>
2020-09-27 22:59:33 -0300

    Comentário por parametro

--- helloworld.py
+++ helloworld.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
+import sys
 
 arquivo = open('file.txt', 'r')
 conteudo = arquivo.readlines()
 data_e_hora_atuais = datetime.now()
-conteudo.append('Action -> ' + data_e_hora_atuais.strftime('%Y-%m-%d %H:%M:%S') + '\n')
+conteudo.append('Action -> ' + data_e_hora_atuais.strftime('%Y-%m-%d %H:%M:%S') + '\n\n')
+conteudo.append(str(sys.argv))
 
 arquivo = open('file.txt', 'w')
 arquivo.writelines(conteudo)
