be3cfdc08a1dab892121fbec608cd780407160d2
unknown <allison@alunos.utfpr.edu.br>
2020-09-28 19:38:57 -0300

    Testing

--- helloworld.py
+++ helloworld.py
@@ -1,14 +1,7 @@
 # -*- coding: utf-8 -*-
-from datetime import datetime
 import sys
 
-arquivo = open('file.txt', 'r')
-conteudo = arquivo.readlines()
-data_e_hora_atuais = datetime.now()
-conteudo.append('Action -> ' + data_e_hora_atuais.strftime('%Y-%m-%d %H:%M:%S') + '\n\n')
-conteudo.append(str(sys.argv))
-
 arquivo = open('file.txt', 'w')
-arquivo.writelines(conteudo)
+arquivo.writelines(str(sys.argv))
 
 arquivo.close()
