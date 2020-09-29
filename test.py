1219f64796136b54f1bfbd5d0acb4312d2261d60
unknown <allison@alunos.utfpr.edu.br>
2020-09-29 17:57:02 -0300

    Testing

--- test.py
+++ test.py
@@ -3,6 +3,4 @@
 import os
 dados_evento = os.environ['GITHUB_CONTEXT']
 
-print(dados_evento['action'])
-print(dados_evento['comment']['body'])
-print(dados_evento['comment']['user']['login'])
+print(dados_evento)
