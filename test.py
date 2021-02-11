010a7a70330707793cb9f76f89e866af001b84ee
unknown <allison@alunos.utfpr.edu.br>
2021-02-11 19:55:55 -0300

    teste

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import os
 import json
 
 dados_evento = json.loads(os.environ['GITHUB_CONTEXT'])
-
-print(dados_evento["action"])
-print(dados_evento["comment"]["body"])
-print(dados_evento["comment"]["user"]["login"])
+print(dados_evento)
+#print(dados_evento["action"])
+#print(dados_evento["comment"]["body"])
+#print(dados_evento["comment"]["user"]["login"])
