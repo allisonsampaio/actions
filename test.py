efb0dec04b55683c0fcc272e7d7e7b1729fcb1b2
allison <allison@alunos.utfpr.edu.br>
2021-02-14 16:52:37 -0300

    teste

--- test.py
+++ test.py
@@ -7,10 +7,11 @@ dados_evento = json.loads(os.environ['GITHUB_CONTEXT'])
 filename = "file.json"
 
 with open(filename, "r+") as out_file:
-    data = json.load(out_file)
-    data[0].append(dados_evento)
+    data = json.loads(out_file)
+    data.update(dados_evento)
 
-    json.dump(data, out_file, indent=4)
+    print(json.dumps(data))
+    json.dumps(data, out_file, indent=4)
 
 # print(dados_evento["action"])
 # print(dados_evento["comment"]["body"])
