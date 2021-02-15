5b72c515466b7b8bd4c31c5e59c9eec2ca48f470
allison <allison@alunos.utfpr.edu.br>
2021-02-14 22:14:35 -0300

    teste

--- data/store_events.py
+++ data/store_events.py
@@ -3,12 +3,12 @@
 import os
 import json
 
-def write_json(data, filename='data.json'): 
+def write_json(data, filename='data/data.json'): 
     with open(filename,'w') as f: 
         json.dump(data, f, indent=4) 
       
       
-with open('data.json') as json_file: 
+with open('data/data.json') as json_file: 
     data = json.load(json_file) 
     temp = data['lista_eventos'] 
     novo_evento = json.loads(os.environ['GITHUB_CONTEXT'])
