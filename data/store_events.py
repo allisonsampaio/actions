7ed2b29bf65f2e72e32e567bb8c99cde889c0bae
allison <allison@alunos.utfpr.edu.br>
2021-03-04 21:11:21 -0300

    Filtrando dados das actions

--- data/store_events.py
+++ data/store_events.py
@@ -3,15 +3,41 @@
 import os
 import json
 
-def write_json(data, filename='data/data.json'): 
-    with open(filename,'w') as f: 
-        json.dump(data, f, indent=4) 
-      
-      
-with open('data/data.json') as json_file: 
-    data = json.load(json_file) 
-    temp = data['lista_eventos'] 
-    novo_evento = json.loads(os.environ['GITHUB_CONTEXT'])
+
+def write_json(data, filename='data/data.json'):
+    with open(filename, 'w') as f:
+        json.dump(data, f, indent=4)
+
+
+novo_evento = json.loads(os.environ['GITHUB_CONTEXT'])
+
+with open('data/data.json') as json_file:
+    data = json.load(json_file)
+    temp = data['lista_eventos']
     temp.append(novo_evento)
-      
-write_json(data) 
+write_json(data)
+
+
+with open('users.json') as json_users:
+    users = json.load(json_users)
+    exist_user = False
+
+    for u in users['users']:
+        if(u['id'] == novo_evento['sender']['id']):
+            u['coins'] = u['coins'] + 1
+            exist_user = True
+            with open('users.json', 'w') as js:
+                json.dump(users, js, indent=4)
+            break
+
+    if(exist_user == False):
+        new_user = {
+            "id": novo_evento['sender']['id'],
+            "login": novo_evento['sender']['login'],
+            "avatar_url": novo_evento['sender']['avatar_url'],
+            "html_url": novo_evento['sender']['html_url'],
+            "coins": 1
+        }
+        users['users'].append(new_user)
+        with open('users.json', 'w') as js:
+            json.dump(users, js, indent=4)
--- data/teste.py
+++ data/teste.py
@@ -3,18 +3,38 @@
 import os
 import json
 
-with open('data.json') as json_file:
+with open('events.json') as json_file:
     data = json.load(json_file)
-    temp = data['lista_eventos'] 
-#    for event in temp:
-#        print(event['action'])
-#        print(event['sender']['login'])
-#        print(event['sender']['avatar_url'])
-#        print(event[1])
-#        print('\n')
+    temp = data['events']
+    for event in temp:
+        #print(event['sender']['id'])
+        #print(event['sender']['login'])
+        #print(event['sender']['avatar_url'])
+        #print(event['sender']['html_url'])
+        #print(event['action'])
+        #print(list(event.keys())[1])
+        #print('\n')
 
-    i = 0
-    while(i < len(temp)):
-        print(temp[i])
-        print('\n\n\n\n')
-        i = i + 1
+        with open('users.json') as json_users:
+            users = json.load(json_users)
+            exist_user = False
+
+            for u in users['users']:
+                if(u['id'] == event['sender']['id']):
+                    u['coins'] = u['coins'] + 1
+                    exist_user = True
+                    with open('users.json', 'w') as js:
+                        json.dump(users, js, indent=4)
+                    break
+
+            if(exist_user == False):
+                new_user = {
+                    "id": event['sender']['id'],
+                    "login": event['sender']['login'],
+                    "avatar_url": event['sender']['avatar_url'],
+                    "html_url": event['sender']['html_url'],
+                    "coins": 1
+                }
+                users['users'].append(new_user)
+                with open('users.json', 'w') as js:
+                    json.dump(users, js, indent=4)
